b5f31749b30882dfa03a69ec8bcd73c62ecc58d1
soumya569 <soumyaranjanroul38@gmail.com>
2025-08-16 20:07:53 +0530

    updated with model

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
-    path('about/',views.about, name='about'),
-    path('contact/',views.contact, name='contact'),
-    path('login/',views.contact, name='login'),
-    path('registration/',views.contact, name='registration'),
-
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('prediction/', views.prediction, name='prediction'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -1,23 +1,64 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+import pickle
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import tensorflow as tf
+import os
+
+model = tf.keras.models.load_model("./lemon.h5")
+class_name = ['bad_quality', 'empty_background', 'good_quality']
+
+def read_file_as_image(data):
+    image = np.array(Image.open(BytesIO(data)))
+    return image
 
 # Create your views here.
 def home(request):
-    #return HttpResponse("This is our home page")
-    return render(request,"index.html")
+    # return HttpResponse("This is the our home page")
+    return render(request, 'index.html')
 
 def about(request):
-   # return HttpResponse("This is our about page")
-    return render(request,"about.html")
+    # return HttpResponse("This is the our about page")
+    return render(request, 'about.html')
 
 def contact(request):
-    #return HttpResponse("This is our contact page")
-    return render(request,"contact.html")
+    # return HttpResponse("This is the our contact page")
+    return render(request, 'contact.html')
+
 def login(request):
-    #return HttpResponse("This is our contact page")
-    return render(request,"login.html")
+    # return HttpResponse("This is the our login page")
+    return render(request, 'login.html')
 
 def registration(request):
-    #return HttpResponse("This is our contact page")
-    return render(request,"registration.html")
+    # return HttpResponse("This is the our registration page")
+    return render(request, 'registration.html')
 
+def prediction(request):
+    
+    context = {
+        "status": False
+    }
+    # return HttpResponse("This is the our registration page")
+    if request.method == "POST":
+        f = request.FILES['lemon']
+        # print(str(f))
+        data = f.read()
+        image = read_file_as_image(data)
+        img_batch = np.expand_dims(image, 0)
 
+        predictions = model.predict(img_batch)
+        predicted_class = class_name[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+        # print(predictions, predicted_class, confidence)
+        # print("lemon")
+        context = {
+            "Actual_image": str(f),
+            "confidence": str(confidence * 100),
+            "prediction": str(predicted_class),
+            "status" : True
+        }
+        return render(request, "prediction.html", context)
+    else:
+    
+        return render(request, 'prediction.html', context)
